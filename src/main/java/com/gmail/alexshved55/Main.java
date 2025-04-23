7946de44a64a07131f5812bd680eb1c425b17494
Aleksandr Shvidkii <alexshved55@gmail.com>
2025-04-24 00:46:29 +0300

    added SoftAssertionsTest

---
+++ src/main/java/com/gmail/alexshved55/Main.java
@@ -0,0 +1,7 @@
+package com.gmail.alexshved55;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/SoftAssertionsTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void CheckCodeInside() {
+        open("https://github.com/");
+        $("[placeholder='Search or jump to...']").click();
+        $("input[id='query-builder-test']").setValue("Selenide").pressEnter();
+        $("a[href='/selenide/selenide']").click();
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(text("Soft assertions")).$(byText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("Soft assertions")).click();
+        $$(".markdown-heading").findBy(text("JUnit5")).sibling(0).$("pre").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n"+
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
